5f9aab5594cd32fd2c356698b73940adba32b28f
chinonsoxavier <chinonsoxavier26@gmail.com>
2023-06-04 16:16:18 +0100

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import Netflix from "../assets/projects/netflixClone.png";
+import lamaEcommerce from "../assets/projects/lamaEcommerce.png";
+import envie from "../assets/projects/envie.png";
+import Karbox from "../assets/projects/Karbox.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Netflix app",
+    image: Netflix,
+    github: "",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "React JS Ecommerce app",
+    image: lamaEcommerce,
+    github: "",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "React JS Live Auction app",
+    image: envie,
+    github: "",
+    live: "",
+  },
+  {
+    id: 4,
+    name: "React JS Car tradding app",
+    image: Karbox,
+    github: "",
+    live: "",
+  },
+];
